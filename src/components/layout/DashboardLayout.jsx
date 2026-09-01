b5f1e23a88fcebbdc8805da10fdf3b5c8615344b
cg000als-8877 <cg000als@gmail.com>
2026-09-01 16:57:27 +0600

    2

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -331,7 +331,7 @@ export default function DashboardLayout({ children }) {
 
         {/* Main Content */}
         <main 
-          className="flex-1 overflow-y-auto w-full pt-[65px] pb-16 md:pt-8 md:pb-8 p-4 md:p-8 relative"
+          className="flex-1 overflow-y-auto w-full pt-[65px] pb-16 md:pt-8 md:pb-8 p-4 md:p-8 relative hide-scrollbar md:[scrollbar-width:auto]"
         >
           <div className="w-full max-w-[1800px] mx-auto">
             {children}
